89c3ff18c48af456b1f26c5cfe89a495fb9b98f5
volkue <valdem72@i.ua>
2022-01-12 10:04:33 +0200

    starting Project Keeper

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+import Note from './Note';
+import Footer from './Footer';
+import notes from '../note';
+
+
+
+function App() {
+    return ( <div>
+        <Header />
+       {notes.map(note => <Note 
+           key = {note.key}
+           title = {note.title}
+           content = {note.content}
+            />
+       )}
+        
+        <Footer />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer (){
+    const date = new Date ();
+    const currentYear = date.getFullYear();
+    return (<footer>
+        <p>Copyright ⓒ {currentYear} </p>
+    </footer>);
+}
+
+export default Footer;
